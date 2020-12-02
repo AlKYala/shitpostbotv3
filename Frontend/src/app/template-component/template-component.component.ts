441caea84e71cad60bad052ac420ce77618aebc8
aliyalama@googlemail.com <Gundam1979!>
2020-12-02 14:43:11 +0100

    Added first Form for template

--- Frontend/src/app/template-component/template-component.component.ts
+++ Frontend/src/app/template-component/template-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormControlsSettings} from '../FormControlSettings/form.controls.settings';
 
 @Component({
   selector: 'app-template-component',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-component.component.css']
 })
 export class TemplateComponentComponent implements OnInit {
-
+  public templateForm: FormGroup;
+  public FormControlSettings: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.initImageForm();
+  }
+
+  public initImageForm(): void {
+    this.templateForm = new FormGroup( {
+      url: FormControlsSettings.urlFormControl(),
+      name: FormControlsSettings.nameFormControl()
+    });
+  }
+
+  public get formControls(): any {
+    return this.templateForm.controls;
+  }
+
+  // loads image into cropper
+  public loadImage(): void {
+
   }
 
 }
