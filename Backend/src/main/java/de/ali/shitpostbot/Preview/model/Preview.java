47423b2c13e4ca10c9d3e4bbd5b5bccb429291ca
aliyalama@googlemail.com <Gundam1979!>
2020-12-06 13:25:28 +0100

    Deleted Model for preview to save space Cropped areas will be marked in the gallery

--- Backend/src/main/java/de/ali/shitpostbot/Preview/model/Preview.java
+++
@@ -1,7 +0,0 @@
-package de.ali.shitpostbot.Preview.model;
-
-import de.ali.shitpostbot.shared.model.BaseEntity;
-
-public class Preview extends BaseEntity {
-
-}
--- Backend/src/main/java/de/ali/shitpostbot/Template/model/Template.java
+++ Backend/src/main/java/de/ali/shitpostbot/Template/model/Template.java
@@ -1,3 +1,10 @@
+/*
+Zur diskussion steht:
+Willst du die preview bilder uebernehmen im base64 format?
+Willst du nur Koordinaten uebernehmen und draufzeichen?
+-> Mit verlinkung zum originalen bild?
+ */
+
 package de.ali.shitpostbot.Template.model;
 
 import de.ali.shitpostbot.shared.model.BaseEntity;
@@ -12,7 +19,9 @@ import javax.persistence.Entity;
 @Setter
 @NoArgsConstructor
 public class Template extends BaseEntity {
-
+    /**
+     * Das originale Bild
+     */
     private String baseUrl;
 
 }
