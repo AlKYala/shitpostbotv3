502b3a3b4ac0c3b0dca03cacafca2ca5c56ea0a9
aliyalama@googlemail.com <Gundam1979!>
2020-12-08 00:34:24 +0100

    Added model for user, build relations with other entities

--- Backend/src/main/java/de/ali/shitpostbot/Image/model/Image.java
+++ Backend/src/main/java/de/ali/shitpostbot/Image/model/Image.java
@@ -1,11 +1,15 @@
 package de.ali.shitpostbot.Image.model;
 
+import de.ali.shitpostbot.User.model.User;
 import de.ali.shitpostbot.shared.model.BaseEntity;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.io.Serializable;
 
 @Entity
@@ -14,4 +18,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class Image extends BaseEntity implements Serializable {
     private String url;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    @EqualsAndHashCode.Exclude
+    private User poster;
 }
