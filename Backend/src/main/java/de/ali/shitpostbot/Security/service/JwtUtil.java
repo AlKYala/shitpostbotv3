3687a63312b8762127ae15c1705d5997f3b620f3
aliyalama@googlemail.com <Gundam1979!>
2020-12-08 01:55:17 +0100

    Security WIP

--- Backend/src/main/java/de/ali/shitpostbot/Security/service/JwtUtil.java
+++ Backend/src/main/java/de/ali/shitpostbot/Security/service/JwtUtil.java
@@ -1,16 +1,22 @@
 package de.ali.shitpostbot.Security.service;
 
+import de.ali.shitpostbot.User.model.User;
 import de.ali.shitpostbot.User.service.UserService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
-import static de.ali.shitpostbot.Security.SecurityConstants.SECRET_KEY;
+import static de.ali.shitpostbot.Security.SecurityConstants.*;
 
 @Slf4j
 @Service
@@ -34,4 +40,19 @@ public class JwtUtil {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
 
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<String, Object>();
+        User user = this.userService.findByUsername(userDetails.getUsername());
+        claims.put("isAdmin", user.isAdmin());
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    public String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().
+                setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SIGNATURE_ALGORITHM, SECRET_KEY).compact();
+
+    }
 }
