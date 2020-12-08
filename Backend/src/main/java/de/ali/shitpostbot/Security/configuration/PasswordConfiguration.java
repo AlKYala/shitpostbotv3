b2829b72d0106ce642d094e99b2ff752f47e72c6
aliyalama@googlemail.com <Gundam1979!>
2020-12-08 21:45:44 +0100

    PasswordConfiguration complete

---
+++ Backend/src/main/java/de/ali/shitpostbot/Security/configuration/PasswordConfiguration.java
@@ -0,0 +1,18 @@
+package de.ali.shitpostbot.Security.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class PasswordConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
