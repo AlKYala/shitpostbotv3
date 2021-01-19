8d802a0e7de974af778555df51420b51aaec3330
aliyalama@googlemail.com <Gundam1979!>
2021-01-19 13:17:49 +0100

    implemented template deletion

--- Frontend/src/app/template-item/template-item.component.ts
+++ Frontend/src/app/template-item/template-item.component.ts
@@ -1,18 +1,46 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Template} from '../../shared/template/model/Template';
+import {User} from '../../shared/user/model/User';
+import {LocalStorageService} from '../../shared/services/localstorage.service';
+import {SubscriptionService} from '../../shared/services/subscription.service';
+import {Subscription} from 'rxjs';
+import {TemplateService} from '../../shared/template/service/template.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-template-item',
   templateUrl: './template-item.component.html',
   styleUrls: ['./template-item.component.scss']
 })
-export class TemplateItemComponent implements OnInit {
+export class TemplateItemComponent implements OnInit, OnDestroy {
   @Input() public template: Template;
-
-  constructor() {
+  public templatePoster: User;
+  public subscriptions: Subscription[];
+  constructor(private readonly localStorageService: LocalStorageService,
+              private readonly subscriptionService: SubscriptionService,
+              private readonly templateService: TemplateService,
+              private readonly toastrService: ToastrService) {
   }
-
   ngOnInit(): void {
+    this.subscriptions = [];
+    this.templatePoster = this.template.poster;
   }
-
+  ngOnDestroy(): void {
+    this.subscriptionService.unsubscribeAll(this.subscriptions);
+  }
+  private checkIsUserPoster(): boolean {
+    return this.templatePoster.id === this.localStorageService.getCurrentUserId();
+  }
+  private checkIsUserAdmin(): boolean {
+    return this.localStorageService.getAdminState();
+  }
+  public canDelete(): boolean {
+    return this.checkIsUserAdmin() || this.checkIsUserPoster();
+  }
+  public deleteTemplate(template: Template): void {
+    const subscription: Subscription = this.templateService.delete(template.id).pipe().subscribe((id: number) => {
+      location.reload();
+      this.toastrService.success(`Template with ID ${id} deleted`);
+    });
+  }
 }
